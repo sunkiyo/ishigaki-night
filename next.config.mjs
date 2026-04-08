fc96c31225883061e70548d4c22e52b62924e33e
Kiyoyasu Sakamoto <sunkiyo@gmail.com>
2026-04-08 23:07:50 +0900

    fix: pdf-parseをwebpackバンドル対象外に設定

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,7 @@ const withNextIntl = createNextIntlPlugin('./src/i18n/request.ts')
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: { missingSuspenseWithCSRBailout: false },
+  serverExternalPackages: ['pdf-parse'],
 }
 
 export default withNextIntl(nextConfig)
